5c98ade7f6fd234b8e376c8e57f9e060c6858504
Frank Jimenez <frank_jimenez113@gmail.com>
2022-10-26 14:00:45 -0400

    Component NavBar.jsx added: navbar, searchbar and loging

--- src/components/header/NavBar.jsx
+++ src/components/header/NavBar.jsx
@@ -1,7 +1,31 @@
 import React from 'react'
 
 export const NavBar = () => {
-  return (
-    <div>NavBar</div>
-  )
+    return (
+        <div id='nav-bar'>
+            <div>
+                <ul>
+                    <a href=""><li>PS4</li></a>
+                    <a href=""><li>PS5</li></a>
+                    <a href=""><li>Nintendo Switch</li></a>
+                    <a href=""><li>PC</li></a>
+                </ul>
+            </div>
+            <div id='search-bar'>
+                <input className='search-bar' placeholder='Buscar Juego' />
+                <button className='search-btn'>
+                    Buscar
+                </button>
+                <div className='fav-search'>
+                    Favorite games ❤️ {'5'}
+                </div>
+            </div>
+            <div>
+                <ul>
+                    <li>login</li>
+                    <li>register</li>
+                </ul>
+            </div>
+        </div>
+    )
 }
