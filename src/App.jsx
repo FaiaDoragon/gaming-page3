52cbc316e49c7b60167a42302f40c71bb3b7da08
Frank Jimenez <frank_jimenez113@gmail.com>
2022-10-26 12:31:26 -0400

    start of the project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import reactLogo from './assets/react.svg'
+import { GamingPage } from './components/GamingPage'
+
+
+function App() {
+  
+
+  return (
+    <GamingPage />
+  )
+}
+
+export default App
