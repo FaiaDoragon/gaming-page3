7bf411615c5706fad83f1656d01cc67ad31fe79f
Frank Jimenez <frank_jimenez113@gmail.com>
2022-10-27 08:14:37 -0400

    Content.jsx modified: game cards have been added to the component

--- src/components/boddy/Countent.jsx
+++ src/components/boddy/Countent.jsx
@@ -1,7 +1,27 @@
 import React from 'react'
 
-export const Countent = () => {
+import games from '../../data/games.json'
+
+export const Content = (props) => {
+
+  const { title } = props;
+
   return (
-    <div>Countent</div>
+    <div className='game-content'>
+      {
+        games.map(games => {
+          return (
+            <div className='card-game' key={games.id}>
+              <img key={games.id} src={games.url} alt={games.title} width="225px" height="288px" title={games.title} />
+              <div className='card-game-bottom' >
+                <h3> {games.title}</h3>
+                <button>❤️</button>
+              </div>
+            </div>
+          )
+        })
+
+      }
+    </div >
   )
 }
