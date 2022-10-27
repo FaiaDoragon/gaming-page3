1d7a806e0fca43b875b021dd64f300f9b7b78080
Frank Jimenez <frank_jimenez113@gmail.com>
2022-10-27 08:09:52 -0400

    Components included in GamingPage.jsx: LeftColumn.jsx and RightColumn.jsx

--- src/components/GamingPage.jsx
+++ src/components/GamingPage.jsx
@@ -1,18 +1,28 @@
 import React from 'react'
 import { NavBar } from './header/NavBar'
 import { TopBar } from './header/TopBar'
-import { Countent } from './boddy/Countent'
+import { Content } from './boddy/Countent'
 
 import '../css/topbar.css'
 import '../css/navbar.css'
+import '../css/navbar.css'
+import '../css/content.css'
+
+import { LeftColumn } from './boddy/LeftColumn'
+import { RightColumn } from './boddy/RightColumn'
 
 
 export const GamingPage = () => {
+
   return (
     <>
     <TopBar />
     <NavBar />
-    <Countent />
+    <div id='content'>
+      <LeftColumn />
+      <Content />
+      <RightColumn />
+    </div>
     </>
   )
 }
